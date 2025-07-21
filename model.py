03a51fc76bab88a76b08094c6d57534012e1c055
Zubair <zubiidesign@gmail.com>
2025-07-21 16:44:03 +0500

    first commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+
+   
