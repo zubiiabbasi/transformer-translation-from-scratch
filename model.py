8b9b445ea2c48bc2635a81a6c193cc23737949d1
Zubair <zubiidesign@gmail.com>
2025-07-22 14:16:17 +0500

    Add InputEmbeddings, PositionalEncoding, LayerNormalization, and FeedForward classes

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import math
 class InputEmbeddings(nn.Module):
     def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
@@ -8,5 +8,57 @@ class InputEmbeddings(nn.Module):
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
         
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
 
-   
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model:int,seq_len: int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        #create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        #create a vector of shape (seq_len, 1)
+        position = torch.arange(0,seq_len, dtype = torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float()* (-math.log(10000.0)/d_model))
+
+        #apply sin for even and cos for odd position
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)  #(1, seq_len, d_model)
+
+        self.register_buffer('pe',pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
+        return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #multiplier
+        self.bias = nn.Parameter(torch.zeros(1))  #addition
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        normalized_x = (x - mean) / (std + self.eps)
+        return self.alpha * normalized_x + self.bias
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff) #w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model) #w2 and b2
+
+    def forward(self, x):
+        # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_ff) -> (Batch, Seq_len, d_model)
+        x = self.linear2(self.dropout(torch.relu(self.linear1(x))))
+        return x
+
