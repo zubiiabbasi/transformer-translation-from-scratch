dbe476f4be41517b4f6099f981cb4fe279556203
Zubair <zubiidesign@gmail.com>
2025-07-23 16:27:27 +0500

    Add MultiheadAttentionBlock and ResidualConnection classes

--- model.py
+++ model.py
@@ -62,3 +62,55 @@ class FeedForward(nn.Module):
         x = self.linear2(self.dropout(torch.relu(self.linear1(x))))
         return x
 
+class MultiheadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model must be divisible by h"
+        
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) #Wq
+        self.w_k = nn.Linear(d_model, d_model) #Wk
+        self.w_v = nn.Linear(d_model, d_model) #Wv
+        self.w_o = nn.Linear(d_model, d_model) #Wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query,key,value, mask, dropout: nn.Dropout):
+        d_k = query.shape(-1)
+        # (Batch,h, Seq_len, d_k) -> (Batch,h, Seq_len, Seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9) # (Batch, h, Seq_len, Seq_len)
+        attention_scores = attention_scores.softmax(attention_scores, dim=-1) # (Batch, h, Seq_len, Seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return attention_scores @ value, attention_scores 
+    
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        key = self.w_k(k) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        value = self.w_v(v) # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+
+        # (Batch, Seq_len, d_model) -> (Batch, Seq_len, h, d_k) 
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiheadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, Seq_len, d_k) -> (Batch, Seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0],-1,  self.h * self.d_k)
+
+        return self.w_o(x)  # (Batch, Seq_len, d_model)
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, subLayer):
+        return x + self.dropout(subLayer(self.norm(x)))
