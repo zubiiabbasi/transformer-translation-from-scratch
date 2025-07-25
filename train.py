2397431226361dffa4a73140b7e2bb1991f5d8f1
Zubair <zubiidesign@gmail.com>
2025-07-25 13:12:42 +0500

    Refactor get_dataset function to return dataloaders and tokenizers, and calculate max source and target lengths

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
+from dataset import BilingualDataset, causal_mask
+from model import build_transformer
 
 def get_all_sentences(dataset, language):
     sentences = []
@@ -38,4 +40,31 @@ def get_dataset(config, language):
     validation_dataset_size = len(dataset_raw) - train_dataset_size
     train_dataset_raw , validation_dataset_raw= random_split(dataset_raw, [train_dataset_size, validation_dataset_size])
 
+    train_dataset = BilingualDataset(train_dataset_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    validation_dataset = BilingualDataset(validation_dataset_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+    for item in dataset_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f"Max source length: {max_len_src}, Max target length: {max_len_tgt}")
+
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size= 1, shuffle= True)
+
+    return train_dataloader, validation_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(
+        vocab_src_len,
+        vocab_tgt_len,
+        config['seq_len'],
+        config['seq_len'],
+        config['d_model'],
+    )
+    return model
     
