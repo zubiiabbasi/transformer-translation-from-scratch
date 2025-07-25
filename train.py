b2e775bb5a20ac5af53333becf41e3b19b383e39
Zubair <zubiidesign@gmail.com>
2025-07-25 13:56:09 +0500

    Implement training loop with model saving and TensorBoard logging

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import random_split, DataLoader, Dataset
+from torch.utils.data import random_split, DataLoader
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -9,6 +9,10 @@ from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
 from dataset import BilingualDataset, causal_mask
 from model import build_transformer
+from torch.utils.tensorboard import SummaryWriter
+from config import get_weights_path, get_config
+from tqdm import tqdm
+import warnings
 
 def get_all_sentences(dataset, language):
     sentences = []
@@ -67,4 +71,74 @@ def get_model(config, vocab_src_len, vocab_tgt_len):
         config['d_model'],
     )
     return model
-    
+
+def train_model(config):
+    # define the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, validation_dataloader, tokenizer_src, tokenizer_tgt = get_dataset(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size())
+    model.to(device)
+
+    #Tensorboard
+    write = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps = 1e-9)
+    initial_epoch = 0
+    global_step = 0
+    if config['preload']:
+        model_filename =  get_weights_path(config,config('preload'))
+        print(f"Preloading model {model_filename}")
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_tgt.token_to_id('[PAD]'), label_smoothing= 0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing epoch {epoch:02d}")
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device)  # (batch, seq_len)
+            decoder_input = batch['decoder_input'].to(device)  # (batch seq_len)
+            encoder_mask = batch['encoder_mask'].to(device)  # (batch, 1,1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device)  # (batch, 1, seq_len, seq_len)
+
+            # run the tensor through the model
+            encoder_output = model.encode(encoder_input, encoder_mask)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+            proj_output = model.projection(decoder_output)  # (batch, seq_len, vocab_tgt_len)
+
+            label = batch['label'].to(device)  # (batch, seq_len)
+            #(batch, seq_len, vocab_tgt_len)  --> (batch * seq_len, vocab_tgt_len)
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()))
+            batch_iterator.set_postfix({f"loss": f"{loss.item():6.3f}"})
+
+            #log the loss to tensorboard
+            write.add_scalar('train loss', loss.item(), global_step)
+            write.flush()
+
+            # backpropagation
+            loss.backward()
+            # update the weights
+            optimizer.step()
+            optimizer.zero_grad()
+
+            global_step += 1
+
+        # save the model
+        model_filename = get_weights_path(config, f'{epoch:02d}')
+        torch.save({
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "global_step": global_step,
+        }, model_filename)
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
