88ef9f9b30459b0065b14b4161d0576a704fc5b2
Zubair <zubiidesign@gmail.com>
2025-07-29 11:21:22 +0500

    Update batch size in config and add inference notebook for model validation

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size" : 16,
+        "batch_size" : 8,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len" : 350, 
